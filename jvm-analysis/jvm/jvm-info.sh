551f8574a68616730be1645139bda77e54b48e7f
helloworldwt <971759990@qq.com>
2017-06-29 15:38:09 +0800

    update jvm analysis

--- jvm-analysis/jvm/jvm-info.sh
+++ jvm-analysis/jvm/jvm-info.sh
@@ -3,7 +3,7 @@ ps -Leo pid,lwp,user,pcpu,pmem,cmd >> /tmp/pthreads.log
 echo "ps -Leo pid,lwp,user,pcpu,pmem,cmd >> /tmp/pthreads.log" >> /tmp/pthreads.log
 echo `date` >> /tmp/pthreads.log
 echo 1
-pid=`ps aux|grep tomcat|grep cwh|awk -F ' ' '{print $2}'`
+pid=`ps aux|grep tomcat|grep cwh|awk -F ' ' 'NR==1{print $2}'`
 echo 2
 echo "pstack $pid >> /tmp/pstack.log" >> /tmp/pstack.log
 pstack $pid >> /tmp/pstack.log
